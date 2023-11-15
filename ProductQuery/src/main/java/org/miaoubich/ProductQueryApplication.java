acaa754a188f1e60b4eecf3293faaa6ca3fd3375
Ali Bouzar <ali.bouzar@gmail.com>
2023-11-15 08:19:40 -0600

    Add ProductCommand and ProductQuery microservices

---
+++ ProductQuery/src/main/java/org/miaoubich/ProductQueryApplication.java
@@ -0,0 +1,13 @@
+package org.miaoubich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductQueryApplication.class, args);
+	}
+
+}
